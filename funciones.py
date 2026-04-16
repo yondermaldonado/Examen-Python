ea295097c35cbd2fe6bb974e434d92b88c211dc8
yonder_maldonado <yondermaldonado23@gmail.com>
2026-04-16 16:26:00 -0500

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,165 @@
+import json
+import csv
+import datetime
+ac = "mas_vendido.json"
+def menuPrincipal():
+    print("---------------------------------------")
+    print("SISTEMA DE FACRURACION RESTAURANTE ACME")
+    print("1. Menu productos")
+    print("2. Menu mesas")
+    print("3. Menu clientes")
+    print("4. Crear factura")
+    print("5. Registro de ventas")
+    print("6. Ranking menos vendidos")
+    print("7. Productos mas vendidos")
+    print("8. Salir")
+    print("---------------------------------------")
+    
+def menuProductos():
+    print("---------------------------------------")
+    print("MENU PRODUCTOS")
+    print("1. registrar producto")
+    print("2. ver productos")
+    print("3. Salir")
+    print("---------------------------------------")
+    
+def menuMesas():
+    print("---------------------------------------")
+    print("MENU MESAS")
+    print("1. registrar mesas")
+    print("2. ver mesas")
+    print("3. Salir")
+    print("---------------------------------------")
+    
+def menuClientes():
+    print("---------------------------------------")
+    print("MENU CLIENTES")
+    print("1. registrar clientes")
+    print("2. ver clientes")
+    print("3. Salir")
+    print("---------------------------------------")
+
+def leerArchivo(ruta):
+    try:
+        with open(ruta,"r")as file:
+            return json.load(file)
+    except Exception:
+        return []
+    
+def guardarArchivo(ruta,datos):
+    with open(ruta,"w")as file:
+        json.dump(datos,file,indent=4)
+    
+
+def registrarProductos(listaProductos):
+    diccionarioProductos = {
+        "codigo":input("digite codigo: "),
+        "nombre":input("digite nombre: "),
+        "precio":input("digite precio: "),
+        "iva":input("digite iva: ")
+    }
+    listaProductos.append(diccionarioProductos)
+    guardarArchivo("productos.json",listaProductos)
+    return listaProductos
+    
+def registrarMesas(listaMesas):
+    diccionarioMesas = {
+        "codigo":input("digite codigo: "),
+        "nombre":input("digite nombre: "),
+        "puestos":input("digite puestos: "),
+    }
+    listaMesas.append(diccionarioMesas)
+    guardarArchivo("mesas.json",listaMesas)
+    return listaMesas
+
+def generarReporteCSV(facturasFiltradas, listaProductos):
+    nombre_archivo = "reporte_ventas.csv"
+    encabezados = ["Mesa", "Producto", "Cantidad", "Subtotal", "IVA", "Total"]
+    
+    with open(nombre_archivo, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(encabezados) 
+
+        for factura in facturasFiltradas:
+            mesa = factura["codigoMesa"]
+            
+            for item in factura["productos"]:
+                for p in listaProductos:
+                    if p["codigo"] == item["codigo"]:
+                        precio = float(p["precio"])
+                        iva_porc = float(p["iva"])
+                        cant = int(item["cantidad"])
+                        
+                        subtotal = precio * cant
+                        iva_valor = subtotal * iva_porc
+                        total = subtotal + iva_valor
+                        
+                        writer.writerow([mesa, p["nombre"], cant, subtotal, iva_valor, total])
+    
+    print("---------------------------------------")
+    print(f"¡Listo! Archivo '{nombre_archivo}' creado.")
+    print("---------------------------------------")
+
+def rankingProductosMenosVendidos(listaFacturas, listaProductos):
+    conteo_ranking = []
+
+    for p in listaProductos:
+        diccionario_auxiliar = {
+            "codigo": str(p["codigo"]),
+            "vendidos": 0
+        }
+        conteo_ranking.append(diccionario_auxiliar)
+
+    for factura in listaFacturas:
+        for item in factura["productos"]:
+            codigo_vendido = str(item["codigo"]) 
+            cantidad_vendida = int(item["cantidad"])
+            
+            for producto_conteo in conteo_ranking:
+                if producto_conteo["codigo"] == codigo_vendido:
+                    producto_conteo["vendidos"] = producto_conteo["vendidos"] + cantidad_vendida
+                    break 
+
+    lista_ordenada = sorted(conteo_ranking, key=lambda x: x["vendidos"])
+
+    print("\n---------- RANKING: 5 PRODUCTOS MENOS VENDIDOS ----------")
+    contador = 1
+    for p in lista_ordenada:
+        print(f"{contador}. Código Producto: {p['codigo']} | Unidades Vendidas: {p['vendidos']}")
+        
+        contador = contador + 1
+        if contador > 5:
+            break
+    print("-------------------------------------------------------")
+def productos_mas_vendidos(listafacturas, listaProductos):
+    conteo_mas_vendido = []
+
+    for p in listaProductos:
+        deccionario_auxiliar = {
+            "codigo": str(p["codigo"]),"nombre": str(p["nombre"]),
+            "vendidos": 0
+        }
+        conteo_mas_vendido.append(deccionario_auxiliar)
+    for factura in listafacturas:
+        for item in factura["productos"]:
+            codigo_vendido = str(item["codigo"])
+            cantidad_vendida = int(item["cantidad"])
+
+            for producto_conteo in conteo_mas_vendido:
+                if producto_conteo["codigo"] == codigo_vendido:
+                    producto_conteo["vendidos"] = producto_conteo["vendidos"]+cantidad_vendida
+                    break
+    lista_con_orden = sorted(conteo_mas_vendido,  key= lambda  x: x["vendidos"])
+    print("---------------Producto mas vendidos--------------")
+    print(lista_con_orden[-1])
+    a = lista_con_orden[-1]
+    a["fecha"]= datetime.datetime.now().strftime("%Y-%m-%d")
+    global ac
+    guardarArchivo(ac,a)
+    return
+    #contador = 4
+    #for p in lista_con_orden:
+    #    print(f"{contador}. Codigo: {p['codigo']}, nombre: {p['nombre']}, vendido {p['vendidos']}")
+    #    contador = contador -1
+    #    if contador == -1:
+    #        break
---
+++ main.py
@@ -0,0 +1,149 @@
+import funciones
+import datetime
+mas = funciones.leerArchivo("mas_vendido")
+listaProductos=funciones.leerArchivo("productos.json")
+listaMesas=funciones.leerArchivo("mesas.json")
+listaClientes=funciones.leerArchivo("clientes.json")
+listaFacturas = funciones.leerArchivo("factura.json")
+
+while True:
+    funciones.menuPrincipal()
+    opcionPrincipal = input("Digite la opcion: ")
+    print("---------------------------------------")
+
+    if opcionPrincipal == "1":
+        while True:
+            funciones.menuProductos()
+            opcionProductos = int(input("Digite la opcion: "))
+            print("---------------------------------------")
+            if(opcionProductos==1):
+                listaProductos = funciones.registrarProductos(listaProductos)
+                    
+            elif(opcionProductos==2):
+                for producto in listaProductos:
+                    print(producto)
+            elif(opcionProductos==3):
+                "saliendo...."
+                break
+
+    elif opcionPrincipal == "2":
+        while True:
+            funciones.menuMesas()
+            opcionMesas = int(input("Digite la opcion: "))
+            print("---------------------------------------")
+            if(opcionMesas==1):
+                funciones.registrarMesas(listaMesas)
+                    
+            elif(opcionMesas==2):
+                for mesas in listaMesas:
+                    print(mesas)
+            elif(opcionMesas==3):
+                "saliendo...."
+                break
+    
+    elif opcionPrincipal == "3":
+        while True:
+            funciones.menuClientes()
+            opcionClientes = int(input("Digite la opcion: "))
+            print("---------------------------------------")
+            if(opcionClientes==1):
+                diccionarioClientes= {
+                    "identificacion":input("digite identificacion: "),
+                    "nombre":input("digite nombre: "),
+                    "telefono":input("digite telefono: ")
+                }
+                listaClientes.append(diccionarioClientes)
+                funciones.guardarArchivo("clientes.json",listaClientes)
+                    
+            elif(opcionClientes==2):
+                for clientes in listaClientes:
+                    print(clientes)
+            elif(opcionClientes==3):
+                "saliendo...."
+                break
+    elif opcionPrincipal == "4":
+        listaFactura = funciones.leerArchivo("factura.json")
+        
+        listaProductosFactura = []
+        print("facturacion")
+        codigoMesa= input("digite codigo de la mesa: ")
+        cliente= input("digite identificacion del cliente: ")
+        
+        while True:
+            codigoProducto= input("digite codigo del producto: ")
+            for producto in listaProductos:
+                if(producto["codigo"]==codigoProducto):
+                    cantidad= input("digite cantidad: ")
+                    productoFactura= {
+                        "codigo": codigoProducto,
+                        "cantidad": cantidad
+                    }
+                    listaProductosFactura.append(productoFactura)
+                    break 
+            
+            opcionFactura= input("quieres agregar mas productos s/n: ")
+            if(opcionFactura=="n"):
+                break
+        
+        diccionarioFactura={
+            "fecha": datetime.datetime.now().strftime("%Y-%m-%d"),
+            "cliente": cliente,
+            "codigoMesa": codigoMesa,
+            "productos": listaProductosFactura
+        }
+        
+        print("----------Factura----------")
+        print(f"Fecha: {diccionarioFactura['fecha']}")
+        print(f"Cliente: {diccionarioFactura['cliente']}")
+        print(f"Codigo Mesa: {diccionarioFactura['codigoMesa']}")
+        print(f"Productos: {diccionarioFactura['productos']}")
+        
+        listaFactura.append(diccionarioFactura)
+        funciones.guardarArchivo("factura.json", listaFactura)
+    elif opcionPrincipal == "5":
+        print("--- REPORTE DE VENTAS POR FECHA ---")
+        fecha_inicio = input("Digite fecha de inicio (AAAA-MM-DD): ")
+        fecha_fin = input("Digite fecha de fin (AAAA-MM-DD): ")
+        
+        todas_las_facturas = funciones.leerArchivo("factura.json")
+        
+        facturas_seleccionadas = []
+        
+        for factura in todas_las_facturas:
+            if fecha_inicio <= factura["fecha"] <= fecha_fin:
+                facturas_seleccionadas.append(factura)
+        
+        if len(facturas_seleccionadas) == 0:
+            print("No se encontraron facturas en ese rango de fechas.")
+        else:
+            funciones.generarReporteCSV(facturas_seleccionadas, listaProductos)
+    elif opcionPrincipal == "6":
+        
+        todas_las_facturas = funciones.leerArchivo("factura.json")
+        
+        if len(todas_las_facturas) == 0:
+            print("Aún no hay facturas para hacer un ranking.")
+        else:
+            funciones.rankingProductosMenosVendidos(todas_las_facturas, listaProductos)
+    elif opcionPrincipal == "7":
+        fecha_inicio = input("Digite fecha de inicio (AAAA-MM-DD): ")
+        fecha_fin = input("Digite fecha de fin (AAAA-MM-DD): ")
+        todas_las_facturas = funciones.leerArchivo("factura.json")
+        facturas_seleccionadas = []
+        
+        for factura in todas_las_facturas:
+            if fecha_inicio <= factura["fecha"] <= fecha_fin:
+                facturas_seleccionadas.append(factura)
+        
+        if len(facturas_seleccionadas) == 0:
+            print("No se encontraron facturas en ese rango de fechas.")
+        else:
+            if len(todas_las_facturas) == 0:
+                print("No hay facturas")
+            else:
+                funciones.productos_mas_vendidos(todas_las_facturas, listaProductos)
+    elif opcionPrincipal == "8":
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opcion no valida")
